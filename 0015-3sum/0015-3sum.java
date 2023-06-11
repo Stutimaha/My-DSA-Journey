96740ba9bc42e7e4baddf76993f4ead9d38cf461
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-11 19:59:03 +0530

    Time: 39 ms (82.97%), Space: 51.7 MB (11.79%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,39 @@
+// sort
+//loop
+//b+c=(-a)
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        List<List<Integer>> res = new LinkedList<>();
+        
+        for (int i = 0; i<nums.length-2;i++){//i will be for a here
+            if(i == 0 || i>0 && nums[i]!=nums[i-1]){
+                int low = i+1; //  will be b here
+                int high = nums.length-1; // will be c here
+                int sum = 0 - nums[i]; //b+c=(-a)
+                
+                while(low<high){
+                    if (nums[low] + nums[high] == sum){
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        
+                        // to  avoid dulicate sets
+                        while(low<high && nums[low]==nums[low+1]) low++;
+                        while(low<high && nums[high]==nums[high-1]) high--;
+                        
+                        low++;
+                        high--;                        
+                    }
+                    else if (nums[low]+nums[high]<sum) low++;
+                    else high--;
+                }
+                
+            }
+        }
+        return res;
+    }
+}
+
+
+
