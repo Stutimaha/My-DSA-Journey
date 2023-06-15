307d936c45ce8ad59688d3980b4248ebfbabdca2
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-15 14:43:49 +0530

    Time: 11 ms (95.44%), Space: 48 MB (28.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        ArrayList<Integer> count = new ArrayList<>(map.values());
-        
-        Collections.sort(count, Collections.reverseOrder());
-        
-        int[]  result = new int[k];
-        int i = 0;
-        
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (count.indexOf(entry.getValue()) < k) {
-                result[i] = entry.getKey();
-                i++;
+
+        List<Integer>[] frequencyLists = new List[nums.length + 1];
+        for (int key : map.keySet()) {
+            int frequency = map.get(key);
+            if (frequencyLists[frequency] == null) {
+                frequencyLists[frequency] = new ArrayList<>();
+            }
+            frequencyLists[frequency].add(key);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = frequencyLists.length - 1; i >= 0 && index < k; i--) {
+            if (frequencyLists[i] != null) {
+                for (int num : frequencyLists[i]) {
+                    result[index++] = num;                    
+                }
             }
         }
         return result;
