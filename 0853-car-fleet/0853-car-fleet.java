1c1a3612b250ebc2262850450543a55c47ee2cb0
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-16 11:34:57 +0530

    Time: 141 ms (23.63%), Space: 56.7 MB (43.52%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,25 +1,24 @@
 class Solution {
+
     public int carFleet(int target, int[] position, int[] speed) {
-        int n=position.length;
-        Integer[][] cars = new Integer[n][2];
-        
-        for(int i =0;i<n;i++){
-            cars[i][0]=position[i];
-            cars[i][1]=speed[i];
-        }
-        
+        if (position.length == 1) return 1;
         Stack<Double> stack = new Stack<>();
-        
-        Arrays.sort(cars, java.util.Comparator.comparingInt(o -> o[0]));  //IMPORTANT
-        
-        for (int i = n-1; i>=0 ; i--) {
-            double current_time= (double)(target-cars[i][0])/cars[i][1];
-            if(!stack.isEmpty() && current_time<=stack.peek()){
-                continue;
-            }
-            else stack.push(current_time);
+        int[][] combine = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            combine[i][0] = position[i];
+            combine[i][1] = speed[i];
         }
-        
+
+        Arrays.sort(combine, java.util.Comparator.comparingInt(o -> o[0]));
+        for (int i = combine.length - 1; i >= 0; i--) {
+            double currentTime = (double) (target - combine[i][0]) /
+            combine[i][1];
+            if (!stack.isEmpty() && currentTime <= stack.peek()) {
+                continue;
+            } else {
+                stack.push(currentTime);
+            }
+        }
         return stack.size();
     }
 }
