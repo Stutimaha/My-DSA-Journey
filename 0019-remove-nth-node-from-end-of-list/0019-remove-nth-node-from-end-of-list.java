80de04b7bdcc3eb9c1bed17ef2414aba6541b3fe
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-17 10:41:29 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (24.58%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,38 +1,29 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // Handle the case when the head node is null
-        if (head == null) {
-            return null;
-        }
-        
-        // Create a dummy node to handle the case when the target node is the head node
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        
-        ListNode curr = head;
-        int length = 0;
-        
-        // Calculate the length of the linked list
-        while (curr != null) {
-            length++;
-            curr = curr.next;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        // Move the fast pointer n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            if (fast == null) {
+                // Handle the case when n is greater than the length of the linked list
+                return head;
+            }
+            fast = fast.next;
         }
-        
-        // Handle the case when n is equal to the length of the linked list
-        if (n == length) {
-            return head.next;
+
+        // Move both slow and fast pointers until fast reaches the end
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
-        
-        ListNode removeNext = head;
-        
-        // Find the node before the target node to be removed
-        for (int i = 0; i < length - n-1; i++) {
-            removeNext = removeNext.next;
-        }
-        
-        // Remove the target node
-        removeNext.next = removeNext.next.next;
-        
-        return head;
+
+        // Remove the nth node from the end
+        slow.next = slow.next.next;
+
+        return dummy.next;
     }
 }
