1086b67eee12d3545ab2bfaa25430eccd0ceab73
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-19 00:04:59 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.09%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,17 +1,58 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int haylength=haystack.length();
-        int needlelength=needle.length();
-        if(haylength<needlelength)
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+
+        if (needleLength == 0) {
+            return 0;
+        }
+
+        if (haystackLength < needleLength) {
             return -1;
-        for(int i=0;i<=haystack.length()-needle.length();i++){
-            int j=0;
-            while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
+        }
+
+        int[] lps = computeLPS(needle);
+
+        int i = 0, j = 0;
+        while (i < haystackLength) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
                 j++;
-            if(j==needle.length()){
-                return i;
+                if (j == needleLength) {
+                    return i - j;
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
+
         return -1;
+    }
+
+    private int[] computeLPS(String needle) {
+        int needleLength = needle.length();
+        int[] lps = new int[needleLength];
+        int i = 1, len = 0;
+
+        while (i < needleLength) {
+            if (needle.charAt(i) == needle.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
     }
 }
