1bb72401b91629f675e346e0a2d74cf3320f6b9b
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-20 18:08:09 +0530

    Time: 10 ms (91.38%), Space: 60.4 MB (72.49%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        // When a single element is considered then its averafge will be the number itself only.
+        // When a single element is considered then its average will be the number itself only.
         if (k == 0) {
             return nums;
         }
@@ -9,25 +9,26 @@ class Solution {
         int[] averages = new int[n];
         Arrays.fill(averages, -1);
 
-        // Any index will not have 'k' elements in it's left and right.
+        // Any index will not have 'k' elements in its left and right.
         if (2 * k + 1 > n) {
             return averages;
         }
 
-        // Generate 'prefix' array for 'nums'.
-        // 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] + nums[i];
+        // First get the sum of first window of the 'nums' array.
+        long windowSum = 0;
+        for (int i = 0; i < (2 * k + 1); ++i) {
+            windowSum += nums[i];
         }
+        averages[k] = (int) (windowSum / (2 * k + 1));
 
-        // We iterate only on those indices which have atleast 'k' elements in their left and right.
-        // i.e. indices from 'k' to 'n - k'
-        for (int i = k; i < (n - k); ++i) {
-            int leftBound = i - k, rightBound = i + k;
-            long subArraySum = prefix[rightBound + 1] - prefix[leftBound];
-            int average = (int) (subArraySum / (2 * k + 1));
-            averages[i] = average;
+        // Iterate on rest indices which have at least 'k' elements 
+        // on its left and right sides.
+        for (int i = (2 * k + 1); i < n; ++i) {
+            // We remove the discarded element and add the new element to get current window sum.
+            // 'i' is the index of new inserted element, and
+            // 'i - (window size)' is the index of the last removed element.
+            windowSum = windowSum - nums[i - (2 * k + 1)] + nums[i];
+            averages[i - k] = (int) (windowSum / (2 * k + 1));
         }
 
         return averages;
