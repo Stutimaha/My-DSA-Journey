dafc14d7961390cf79a08f96276c5a8f76ad06cc
Stutimaha <94779689+Stutimaha@users.noreply.github.com>
2023-06-22 16:01:56 +0530

    Time: 26 ms (42.90%), Space: 43.8 MB (11.66%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,31 +1,28 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        HashSet<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
         
-        char[] ch = s.toCharArray();
+        int maxCount = 0;
         int count = 0;
+        
         for (int i = 0; i < k; i++) {
-            if (set.contains(ch[i])) {
+            if (vowels.contains(s.charAt(i))) {
                 count++;
             }
-        } 
+        }
+        maxCount = count;
         
-        int maxCount = count;
-        for (int i = k; i < ch.length; i++) {
-            if ((set.contains(ch[i]) && set.contains(ch[i - k])) || (!set.contains(ch[i]) && !set.contains(ch[i - k]))) {
-                // count remains the same
-            } else {
-                if (set.contains(ch[i - k])) {
-                    count--;
-                }
-                if (set.contains(ch[i])) {
-                    count++;
-                }
+        for (int i = k; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i - k))) {
+                count--;
+            }
+            if (vowels.contains(s.charAt(i))) {
+                count++;
             }
             maxCount = Math.max(maxCount, count);
         }
