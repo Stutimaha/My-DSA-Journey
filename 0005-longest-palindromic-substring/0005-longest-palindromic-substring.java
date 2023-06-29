a16cfae2dc2c510afe6ecfe4af976b24e2ec9d06
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-06-29 23:40:08 +0530

    Time: 23 ms (73.89%), Space: 40.9 MB (99.29%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,25 @@
+class Solution {
+    int maxLen = 0;
+    int start=0;
+    
+    public String longestPalindrome(String s) {
+        String res = "";
+       
+        for(int i = 0; i<s.length();i++){
+            extendPalindrome(s, i, i);
+            extendPalindrome(s,i,i+1);
+        }
+        return s.substring(start, start+maxLen);
+    }
+    
+    public void extendPalindrome(String s, int left, int right){
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        if (maxLen< right- left-1){
+            start = left+1;
+            maxLen = right-left-1;
+        }
+    }
+}
