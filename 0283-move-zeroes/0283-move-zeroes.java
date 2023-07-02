c32df3f748c56b1cd486029c00bc40bd543780ec
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-02 23:23:08 +0530

    Time: 2 ms (40.88%), Space: 45.3 MB (38.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,15 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int i = 0;
-        int j = 0;
-        
-        while (i < nums.length) {
-            j = i + 1;
-            
-            if (nums[i] == 0) {
-                while (j < nums.length && nums[j] == 0) {
-                    j++;
-                }
-                
-                if (j < nums.length) {
-                    swap(nums, i, j);
-                }
+     public void moveZeroes(int[] nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
             }
-            
-            i++;
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
         }
-    }
-    
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
