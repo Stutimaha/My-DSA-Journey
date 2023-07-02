bd089698a425447a7fbbc4a9af219c8c722ddd21
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-02 23:26:53 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (60.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
-     public void moveZeroes(int[] nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.length;i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0; 
+        
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos] = num;
+                insertPos++;
             }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
-            }
+        }
+        
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
         }
     }
 }
