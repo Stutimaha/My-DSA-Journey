ead84add7e29339d7de80efd6fffa48ab08dd16b
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-15 20:30:25 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (97.50%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,21 @@
+import java.util.HashMap;
+
 class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public Solution() {
+        map.put(0, 0);
+        map.put(1, 1);
+    }
+
     public int fib(int n) {
-        if(n<=1){
-            return n;
+        if (map.containsKey(n)) {
+            return map.get(n);
         }
-        
-        int answer = fib(n-1)+fib(n-2);
-        return answer;
+
+        int result = fib(n - 1) + fib(n - 2);
+        map.put(n, result);
+
+        return result;
     }
 }
