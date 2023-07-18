6541538a4aaddd8eb13aa262cf98dcee16fbefe4
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-19 01:40:52 +0530

    Time: 3 ms (83.84%), Space: 44.3 MB (31.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,71 +1,58 @@
-public class Solution {
+class Solution {
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = '.';
             }
         }
         
-        List<List<String>> res = new ArrayList<>();
-        placeQueens(board, 0, res);
+        List<List<String>> result = new ArrayList<>();
         
-        return res;
+        placeQueens(board, 0, result);
+        return result;
     }
     
-    private void placeQueens(char[][] board, int rowIndex, List<List<String>> res) {
-        // Base case: If all queens are placed successfully, add the current configuration to the result
-        if (rowIndex == board.length) {
-            res.add(construct(board));
+    private void placeQueens(char[][] board, int rowIndex, List<List<String>> result){
+        if(rowIndex == board.length){
+            result.add(construct(board));
             return;
         }
         
-        // Try placing a queen in each column of the current row
-        for (int colIndex = 0; colIndex < board.length; colIndex++) {
-            if (isSafe(board, rowIndex, colIndex)) {
-                board[rowIndex][colIndex] = 'Q';  // Place the queen
-                
-                // Recursively try placing the queens in the next row
-                placeQueens(board, rowIndex + 1, res);
-                
-                board[rowIndex][colIndex] = '.';  // Remove the queen to backtrack and explore other possibilities
+        for(int colIndex = 0; colIndex<board.length; colIndex++){
+            if(isSafe(board, rowIndex, colIndex)){
+                board[rowIndex][colIndex]='Q';
+                placeQueens(board, rowIndex+1, result);
+                board[rowIndex][colIndex]='.';
             }
         }
     }
     
-    private boolean isSafe(char[][] board, int row, int col) {
-        // Check if there is a queen in the same column or in any of the previous rows
-        for (int prevRow = 0; prevRow < row; prevRow++) {
-            if (board[prevRow][col] == 'Q') {
+    private boolean isSafe(char[][] board, int rowIndex, int colIndex){
+        for (int prevRow = 0; prevRow < rowIndex; prevRow++) {
+            if (board[prevRow][colIndex] == 'Q') {
                 return false;
             }
         }
         
-        // Check if there is a queen in the upper-left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
+        for(int i = rowIndex-1, j = colIndex-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q') return false;
         }
         
-        // Check if there is a queen in the upper-right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
+        for(int i = rowIndex-1, j = colIndex+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j]=='Q') return false;
         }
-        
-        return true;  // It is safe to place a queen at the given position
+        return true;
     }
     
-    private List<String> construct(char[][] board) {
+    private List<String> construct(char[][] board){
         List<String> res = new LinkedList<>();
-        
-        for (int i = 0; i < board.length; i++) {
+         for (int i = 0; i < board.length; i++) {
             String row = new String(board[i]);
             res.add(row);
         }
-        
         return res;
     }
+    
 }
