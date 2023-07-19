9f180fe319505c4e512a39419f50fc8f0c2d3579
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-19 19:57:09 +0530

    Time: 15 ms (59.03%), Space: 40.3 MB (42.00%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,49 +1,32 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
+     public void solveSudoku(char[][] board) {
+        doSolve(board, 0, 0);
     }
-
-    private boolean solve(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    for (char num = '1'; num <= '9'; num++) {
-                        if (isSafe(board, row, col, num)) {
-                            board[row][col] = num;
-                            
-                            if (solve(board)) {
-                                return true;
-                            } else {
-                                board[row][col] = '.';
-                            }
-                        }
+    
+    private boolean doSolve(char[][] board, int row, int col) {
+        for (int i = row; i < 9; i++, col = 0) { 
+            for (int j = col; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (doSolve(board, i, j + 1))
+                            return true;
+                        board[i][j] = '.';
                     }
-                    
-                    return false;
                 }
-            }
-        }
-        
-        return true;
-    }
-
-    private boolean isSafe(char[][] board, int row, int col, char num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num) {
                 return false;
             }
         }
-
-        int subgridStartRow = 3 * (row / 3);
-        int subgridStartCol = 3 * (col / 3);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[subgridStartRow + i][subgridStartCol + j] == num) {
-                    return false;
-                }
-            }
-        }
-
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; 
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num || 
+                    board[blkrow + i / 3][blkcol + i % 3] == num)
+                return false;
         return true;
     }
 }
