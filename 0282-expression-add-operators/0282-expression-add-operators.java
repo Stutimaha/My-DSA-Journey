edc6682072b8414111754bed37d7f344d3fee822
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-20 15:54:27 +0530

    Time: 139 ms (50.87%), Space: 45.3 MB (7.69%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,28 +1,27 @@
-public class Solution {
-    public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
+class Solution {
+    List<String> ans = new ArrayList<>();
+    String s;
+    int target;
+    public List<String> addOperators(String s, int target) {
+        this.s = s;
+        this.target = target;
+        backtrack( 0, "", 0, 0);
+        return ans;
     }
-    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
-        if(pos == num.length()){
-            if(target == eval)
-                rst.add(path);
+    void backtrack(int i, String path, long resSoFar, long prevNum) {
+        if (i == s.length()) {
+            if (resSoFar == target) ans.add(path);
             return;
         }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
-            }
-            else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
-                
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
-                
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+        for (int j = i; j < s.length(); j++) {
+            if (j > i && s.charAt(i) == '0') break; 
+            long num = Long.parseLong(s.substring(i, j + 1));
+            if (i == 0) {
+                backtrack(j + 1, path + num, num, num); 
+            } else {
+                backtrack(j + 1, path + "+" + num, resSoFar + num, num);
+                backtrack(j + 1, path + "-" + num, resSoFar - num, -num);
+                backtrack(j + 1, path + "*" + num, resSoFar - prevNum + prevNum * num, prevNum * num); 
             }
         }
     }
