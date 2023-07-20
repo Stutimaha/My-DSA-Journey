175510bf67acdd2f5d6ddf858c961354aa2ee010
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-20 23:18:14 +0530

    Time: 139 ms (50.87%), Space: 44.6 MB (70.10%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,27 +1,35 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    String s;
-    int target;
-    public List<String> addOperators(String s, int target) {
-        this.s = s;
-        this.target = target;
-        backtrack( 0, "", 0, 0);
-        return ans;
+    public List<String> addOperators(String num, int target) {
+        List<String> result = new ArrayList<String>();
+        
+        if(num.length()==0 || num==null){
+            return result;
+        }
+        
+        backtrack(result, "", num, target, 0 , 0, 0);
+        return result;
     }
-    void backtrack(int i, String path, long resSoFar, long prevNum) {
-        if (i == s.length()) {
-            if (resSoFar == target) ans.add(path);
+    
+    private void backtrack (List<String> result, String path, String num, int target, int pos, long eval, long multed){
+        if(pos==num.length()){
+            if(target==eval)
+                result.add(path);
             return;
         }
-        for (int j = i; j < s.length(); j++) {
-            if (j > i && s.charAt(i) == '0') break; 
-            long num = Long.parseLong(s.substring(i, j + 1));
-            if (i == 0) {
-                backtrack(j + 1, path + num, num, num); 
-            } else {
-                backtrack(j + 1, path + "+" + num, resSoFar + num, num);
-                backtrack(j + 1, path + "-" + num, resSoFar - num, -num);
-                backtrack(j + 1, path + "*" + num, resSoFar - prevNum + prevNum * num, prevNum * num); 
+        
+        for(int i = pos; i<num.length();i++){
+            if(i!= pos && num.charAt(pos)=='0')
+                break;
+            
+            long current = Long.parseLong(num.substring(pos, i+1));
+            
+            if(pos==0){
+                backtrack(result, path+current, num, target, i+1, current, current);
+            }
+            else{
+                backtrack(result, path+"+"+current, num, target, i+1, eval + current, current);
+                backtrack(result, path+"-"+current, num, target, i+1, eval - current, -current);
+                backtrack(result, path+"*"+current, num, target, i+1, eval - multed + multed * current, multed * current);
             }
         }
     }
