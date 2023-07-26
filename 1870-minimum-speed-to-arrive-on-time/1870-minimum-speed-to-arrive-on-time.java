c27816c8b054fa36ae05c1ce6fa6395e715b0e10
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-07-26 10:50:33 +0530

    Time: 206 ms (65.40%), Space: 59.1 MB (56.40%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -2,33 +2,36 @@ class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
         int n = dist.length;
         int left = 1;
-        int right = 10000007; 
-        int result = -1;
-        
-        int hr = (int) Math.ceil(hour);
-        if(hr<n){
-            return result;
+        int right = 10000007;
+
+        if (hour < n - 1) {
+            return -1;
         }
-        
+
         // Binary search
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             double totalTime = 0.0;
-            
+
             for (int i = 0; i < n - 1; i++) {
-                totalTime += Math.ceil(dist[i] / (double) mid); 
+                totalTime += Math.ceil(dist[i] / (double) mid);
             }
             totalTime += dist[n - 1] / (double) mid;
-            
+
             if (totalTime <= hour) {
-                result = mid;
-                right = mid - 1;
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
 
-        return result;
+        // Check if the last found speed allows you to arrive on time
+        double totalTime = 0.0;
+        for (int i = 0; i < n - 1; i++) {
+            totalTime += Math.ceil(dist[i] / (double) left);
+        }
+        totalTime += dist[n - 1] / (double) left;
+
+        return totalTime <= hour ? left : -1;
     }
 }
-
