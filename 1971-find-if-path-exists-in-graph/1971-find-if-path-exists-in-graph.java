22751863a8442fe0c3bf0e4fa8be74dc510d1ff7
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-08-11 13:32:10 +0530

    Time: 75 ms (76.52%), Space: 166 MB (35.43%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,38 +1,33 @@
 class Solution {
-    public boolean validPath(int totalNodes, int[][] connections, int startNode, int endNode) {
-        List<List<Integer>> graph = buildGraph(totalNodes, connections);
-        boolean[] visited = new boolean[totalNodes];
-        return dfs(graph, visited, startNode, endNode);
+    
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> graph  = buildGraph(n, edges);
+        boolean[] visited = new boolean[n];
+        return dfs(graph, visited, source, destination);
     }
-
-    private boolean dfs(List<List<Integer>> graph, boolean[] visited, int currentNode, int endNode) {
-        if (currentNode == endNode) {
-            return true; 
+    
+    public boolean dfs(List<List<Integer>> graph, boolean[] visited, int source, int destination){
+        if(source == destination){
+            return true;
         }
-
-        visited[currentNode] = true;
-
-        for (int neighbor : graph.get(currentNode)) {
-            if (!visited[neighbor] && dfs(graph, visited, neighbor, endNode)) {
-                return true; 
+        visited[source]=true;
+        for (int neighbor : graph.get(source)) {
+            if(!visited[neighbor] && dfs(graph, visited, neighbor, destination)){
+                return true;
             }
         }
-
-        return false; 
+        return false;
     }
-
-   private List<List<Integer>> buildGraph(int totalNodes, int[][] connections) {
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < totalNodes; i++) {
+    
+    public List<List<Integer>> buildGraph(int n, int[][] edges){
+        List<List<Integer>> graph  = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-
-        for (int[] edge : connections) {
+        for(int[] edge: edges){
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
-
         return graph;
     }
 }
