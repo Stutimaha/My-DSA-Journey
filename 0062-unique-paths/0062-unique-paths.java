4d603429f9e58928957de792fff39c6aa5124caf
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-09-03 15:17:29 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (66.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] grid = new int[m][n];
-        return backtrack(0, 0, m, n, grid);
+  public int uniquePaths(int m, int n) {
+      return uniquePathsHelper(m - 1, n - 1, new int[n][m]);
+  }
+  
+  private int uniquePathsHelper(int m, int n, int[][] memo) {
+      if (m < 0 || n < 0) {
+          return 0;
+      } 
+      else if (m == 0 || n == 0) {
+          return 1;
+      } 
+      else if (memo[n][m] > 0) {
+          return memo[n][m];
+      } 
+      else {
+          memo[n][m] = uniquePathsHelper(m - 1, n, memo) + uniquePathsHelper(m, n - 1, memo);
+    return memo[n][m];
     }
-
-    private int backtrack(int row, int col, int m, int n, int[][] grid) {
-        if (row == m - 1 && col == n - 1) {
-            return 1;
-        }
-
-        if (row >= m || col >= n) {
-            return 0;
-        }
-
-        if (grid[row][col] == 0) {
-            int rightPaths = backtrack(row, col + 1, m, n, grid);
-            int downPaths = backtrack(row + 1, col, m, n, grid);
-            grid[row][col] = rightPaths + downPaths; 
-        }
-
-        return grid[row][col];
-    }
-}
-
-
+  }
+} 
