f27818ccbb1767fc2709a09aa833b14fcddf1841
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-09-05 17:38:19 +0530

    Time: 1 ms (90.32%), Space: 40.4 MB (66.36%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -1,51 +1,32 @@
 class Solution {
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
-        int[] letterCount = new int[26];
-        for (char letter : letters) {
-            letterCount[letter - 'a']++;
+        if (words == null || words.length == 0 || letters == null || letters.length == 0 || score == null || score.length == 0) return 0;
+        int[] count = new int[score.length];
+        for (char ch : letters) {
+            count[ch - 'a']++;
         }
-
-        int n = words.length;
-        int maxScore = 0;
-
-        int[] wordScores = new int[n];
-        for (int i = 0; i < n; i++) {
-            int wordScore = 0;
-            for (char c : words[i].toCharArray()) {
-                wordScore += score[c - 'a'];
+        int res = backtrack(words, count, score, 0);
+        return res;
+    }
+    int backtrack(String[] words, int[] count, int[] score, int index) {
+        int max = 0;
+        for (int i = index; i < words.length; i++) {
+            int res = 0;
+            boolean isValid = true;
+            for (char ch : words[i].toCharArray()) {
+                count[ch - 'a']--;
+                res += score[ch - 'a'];
+                if (count[ch - 'a'] < 0) isValid = false;
             }
-            wordScores[i] = wordScore;
-        }
-
-        for (int mask = 0; mask < (1 << n); mask++) {
-            int[] tempLetterCount = letterCount.clone();
-            int subsetScore = 0;
-            boolean validSubset = true;
-
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    subsetScore += wordScores[i];
-                    for (char c : words[i].toCharArray()) {
-                        int index = c - 'a';
-                        if (tempLetterCount[index] > 0) {
-                            tempLetterCount[index]--;
-                        } else {
-                            validSubset = false;
-                            break;
-                        }
-                    }
-                }
-
-                if (!validSubset) {
-                    break;
-                }
+            if (isValid) {
+                res += backtrack(words, count, score, i + 1);
+                max = Math.max(res, max);
             }
-
-            if (validSubset) {
-                maxScore = Math.max(maxScore, subsetScore);
+            for (char ch : words[i].toCharArray()) {
+                count[ch - 'a']++;
+                res = 0;
             }
         }
-
-        return maxScore;
+        return max;
     }
 }
