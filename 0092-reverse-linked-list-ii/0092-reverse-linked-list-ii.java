4b1324c1e02e44b54a85c8784e006a9560118a45
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-09-07 20:19:09 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (50.29%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -14,27 +14,22 @@ class Solution {
             return head;
         }
         
-        // Dummy node to simplify the logic
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         
-        // Find the node before the left node
         ListNode prev = dummy;
         for (int i = 0; i < left - 1; i++) {
             prev = prev.next;
         }
         
-        // Reverse the nodes from left to right
-        ListNode curr = prev.next; // the left node
+        ListNode curr = prev.next; 
         for (int i = 0; i < right - left; i++) {
-            // Cut out the next node and insert it after prev
             ListNode next = curr.next;
             curr.next = next.next;
             next.next = prev.next;
             prev.next = next;
         }
         
-        // Return the new head
         return dummy.next;
     }
 }
