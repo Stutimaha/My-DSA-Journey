bf557cafd9667bacd340e2e6368cb0782540b0c6
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2023-09-15 10:38:58 +0530

    Time: 309 ms (46.95%), Space: 65.9 MB (35.56%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -3,7 +3,6 @@ class Solution {
         int n = points.length;
         List<int[]> edges = new ArrayList<>();
         
-        // Create a list of all possible edges with their Manhattan distances
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int distance = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
@@ -11,7 +10,6 @@ class Solution {
             }
         }
         
-        // Sort the edges in ascending order based on their weights
         edges.sort((a, b) -> a[2] - b[2]);
         
         int[] parent = new int[n];
@@ -19,7 +17,6 @@ class Solution {
         int minCost = 0;
         int edgeCount = 0;
         
-        // Kruskal's algorithm to find the MST
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
@@ -33,7 +30,6 @@ class Solution {
                 minCost += weight;
                 edgeCount++;
                 
-                // If we have connected all vertices, we can break early
                 if (edgeCount == n - 1) {
                     break;
                 }
@@ -43,7 +39,6 @@ class Solution {
         return minCost;
     }
     
-    // Helper function to find the parent of a vertex using path compression
     private int find(int[] parent, int node) {
         if (parent[node] == -1) {
             return node;
