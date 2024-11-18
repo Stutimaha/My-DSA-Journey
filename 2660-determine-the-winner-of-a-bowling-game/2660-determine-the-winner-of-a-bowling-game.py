8251bad85b5146af02c1c1899562f22a3fb085da
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2024-11-18 19:17:48 +0530

    Time: 31 ms (21.62%), Space: 11.8 MB (22.41%) - LeetHub

---
+++ 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isWinner(self, player1, player2):
+        """
+        :type player1: List[int]
+        :type player2: List[int]
+        :rtype: int
+        """
+        
+        def cal_score(player):
+            score = 0
+            for i in range(len(player)):
+                if (i - 1 >= 0 and player[i - 1] == 10 or i - 2 >= 0 and player[i - 2] == 10):
+                    score += 2*player[i]
+                else:
+                    score += player[i]
+                    
+            return score
+        
+        score1 = cal_score(player1)
+        score2 = cal_score(player2)
+        
+        if score1>score2:
+            return 1
+        elif score1<score2:
+            return 2
+        else:
+            return 0
+            
+            
