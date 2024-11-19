7a8f2a0565f397d40c0c23bfee553c83ba90f286
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2024-11-19 14:38:24 +0530

    Time: 3 ms (34.98%), Space: 12.5 MB (31.41%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -5,15 +5,15 @@ class Solution(object):
         :rtype: List[str]
         """
         res = []
+        
         for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
+            if i%3 == 0 and i %5 == 0:
                 res.append("FizzBuzz")
-            elif i%3==0:
+            elif i%3 == 0:
                 res.append("Fizz")
-            elif i%5==0:
+            elif i%5 == 0:
                 res.append("Buzz")
             else:
                 res.append(str(i))
                 
         return res
-                
