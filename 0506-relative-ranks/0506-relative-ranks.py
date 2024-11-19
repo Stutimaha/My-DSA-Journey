fde80a1e8e7ec85db444dc86864d1c0e34112351
Stuti Mahajan <94779689+Stutimaha@users.noreply.github.com>
2024-11-19 14:48:52 +0530

    Time: 7 ms (75.62%), Space: 12.4 MB (68.70%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -4,16 +4,18 @@ class Solution(object):
         :type score: List[int]
         :rtype: List[str]
         """
-        sorted_score = sorted(((s,i) for i,s in enumerate(score)), reverse=True)
-        res = [""]*len(score)
+        res = [''] * len(score)
         
-        for rank, (score, idx) in enumerate(sorted_score, start = 1):
-            if rank==1:
+        sorted_scores = sorted(((s,i) for i, s in enumerate(score)), reverse = True)
+        
+        for rank, (score, idx) in enumerate(sorted_scores, start=1):
+            if rank == 1:
                 res[idx] = "Gold Medal"
-            elif rank==2:
+            elif rank == 2:
                 res[idx] = "Silver Medal"
-            elif rank==3:
+            elif rank == 3:
                 res[idx] = "Bronze Medal"
             else:
                 res[idx] = str(rank)
+                
         return res
